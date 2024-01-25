7bf6746a0a70bd8ea61ba39dcf57eed46ab98905
Deen Muhammad <40787426+deenMuhammad@users.noreply.github.com>
2024-01-25 10:13:42 +0900

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.13, < 4.0"
+      version = ">= 3.13, <= 5.33"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
